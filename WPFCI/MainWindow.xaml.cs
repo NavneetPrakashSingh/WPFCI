beca346ce5c903100c81f07a08956ac1174e16cd
NavneetPrakashSingh <navneet_prakash_singh@live.com>
2020-07-12 08:29:24 -0700

    Add project files.

---
+++ WPFCI/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFCI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int a = Convert.ToInt32(first_number.Text);
+            int b = Convert.ToInt32(second_number.Text);
+
+            result.Text = calculate(a, b).ToString();
+        }
+
+        private int calculate(int first_number, int second_number)
+        {
+            return first_number + second_number;
+        }
+    }
+}
